d058ba15b9ba0b8eaa27cafb0aa08c27c69100af
Calvin Yuen <scyuenab@gmail.com>
2023-02-04 16:36:27 +0800

    feat: @Body and @Param

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,12 +8,14 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage() {
+  createMessage(@Body() body: any) {
+    console.log('body', body);
     return 'createMessage';
   }
 
   @Get('/:id')
-  getMessage() {
+  getMessage(@Param('id') id: string) {
+    console.log('id', id);
     return 'getMessage';
   }
 }
